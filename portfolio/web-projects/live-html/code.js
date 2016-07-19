dd4541ef470ea55b724ec6eda1c68075a3568e48
Mark Snyder <wolfendeathx@gmail.com>
2016-07-19 15:12:22 +0000

    Test for live html

---
+++ portfolio/web-projects/live-html/code.js
@@ -0,0 +1,9 @@
+function updateHTML() {
+    document.getElementById("htmldiv").innerHTML = document.getElementById("txtArea").value;
+}
+function updateCSS() {
+    document.getElementById("cssdiv").innerHTML = document.getElementById("txtArea2").value;
+}
+function updateJS() {
+    document.getElementById("jsdiv").innerHTML = document.getElementById("txtArea3").value;
+}
