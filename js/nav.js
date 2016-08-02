ee37eaea3e76818112b17a7019ed955cdae86aca
Mark Snyder <wolfendeathx@gmail.com>
2016-08-02 16:07:01 +0000

    Test

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,10 @@
-$(document).ready(window.onload = function pageload() {
+window.onload = function pageload() {
 
-    document.getElementById('mainNav').innerHTML = '<object type="text/html" data="html/nav.html" class="nav"></object>';
-    
+    //document.getElementById('mainNav').innerHTML = '<object type="text/html" data="html/nav.html" class="nav"></object>';
     if(window.location.href.indexOf('index') > -1) {
+       alert("WHOA");
        $('#home').addClass('active');
+       alert("WHOA2");
     }
     else if(window.location.href.indexOf('portfolio') > -1) {
        document.getElementById('portfolio').className = 'active';
@@ -17,4 +18,4 @@ $(document).ready(window.onload = function pageload() {
     else {
        document.getElementById('home').className = 'active';
     }
-});
+};
